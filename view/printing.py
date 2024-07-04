b14fd167334409fde47a657808b4d785f337018c
Manuel <info@tagirijus.de>
2024-07-04 20:20:54 +0200

    fixed flake8 conventions for printing.py

--- view/printing.py
+++ view/printing.py
@@ -5,14 +5,18 @@ from prompt_toolkit import print_formatted_text
 def print_info(message):
     print_formatted_text(HTML(f'<ansiblue>{message}</ansiblue>'))
 
+
 def print_warning(message):
     print_formatted_text(HTML(f'<ansiyellow>{message}</ansiyellow>'))
+
 
 def print_error(message):
     print_formatted_text(HTML(f'<ansired>{message}</ansired>'))
 
+
 def print_success(message):
     print_formatted_text(HTML(f'<ansigreen>{message}</ansigreen>'))
+
 
 def print_formatted(message):
     print_formatted_text(HTML(message))
