a63b1be62dd214d34b5a09b5fa45495f496c4801
Manuel <info@tagirijus.de>
2024-07-11 18:35:48 +0200

    added missing docs fpr method

--- view/printing.py
+++ view/printing.py
@@ -36,7 +36,14 @@ def print_info(message: str) -> None:
     print_formatted_text(HTML(f'<ansiblue>{message}</ansiblue>'))
 
 
-def print_items_in_columns(items, padding=3):
+def print_items_in_columns(items: list[str], padding: int = 3) -> None:
+    """
+    Prints the given list in equally spread columns.
+
+    Args:
+        items (list): The items to print.
+        padding (int): The padding between the elements. (default: `3`)
+    """
     terminal_width = os.get_terminal_size().columns
     max_item_length = max(len(item) for item in items)
     col_width = max_item_length + padding
