5d3c03680220e80a7efb95ad575d48554da84af1
Manuel <info@tagirijus.de>
2024-07-11 15:24:28 +0200

    config.yaml now can contain a help comment

--- model/settings.py
+++ model/settings.py
@@ -10,6 +10,13 @@ class Settings:
     or program configs.
     """
 
+    ADD_HELP_COMMENT: bool
+    """
+    If True the helping comment of all the settings parameters will be
+    saved into the config.yaml as well. This can get handy, if you
+    want to re-check which setting is for what.
+    """
+
     CONFIGFILE: str
     """
     The path to the config file for the program.
@@ -38,6 +45,9 @@ class Settings:
     """
 
     def __init__(self):
+        # soem base config attributes, which are needed on a
+        # plain core basis; thus they are not being in
+        # the self.default_config() method
         self.DATADIR = os.path.join(os.path.expanduser("~"), '.plainvoice')
         self.CONFIGFILE = os.path.join(self.DATADIR, 'config.yaml')
         self.PROJECT_PATH = os.path.dirname(
@@ -49,6 +59,7 @@ class Settings:
         """
         Set the default config.
         """
+        self.ADD_HELP_COMMENT = True
         self.EDITOR = 'vi'
         self.DEFAULT_DUE_DAYS = 14
 
@@ -70,9 +81,38 @@ class Settings:
             dict: The dictionary containing the config data.
         """
         return {
+            'ADD_HELP_COMMENT': self.ADD_HELP_COMMENT,
             'EDITOR': self.EDITOR,
             'DEFAULT_DUE_DAYS': self.DEFAULT_DUE_DAYS
         }
+
+    def get_help_comment(self) -> str:
+        """
+        Return the help comment as a string. It should
+        explain, what are the attributes / variables
+        for etc.
+
+        Returns:
+            str: The help comment as a string.
+        """
+        return """
+# This is the config file for plainvoice. Below are details
+# about the possible settings.
+#
+#
+# ADD_HELP_COMMENT:
+#    If True, this text here always will be added to the
+#    config.yaml at the beginning of the file.
+#
+# EDITOR:
+#    Sets the terminal command for the editor to use, when
+#    editing files. By default it is 'vi'.
+#
+# DEFAULT_DUE_DAYS:
+#    The default due days to use, when calculating the due
+#    date for invoices. By default it is '14'.
+
+""".lstrip()
 
     def init_config(self) -> None:
         """
@@ -95,7 +135,14 @@ class Settings:
         Args:
             config_data (dict): The new config data as a dict.
         """
-        self.EDITOR = config_data.get('EDITOR', self.EDITOR)
+        self.ADD_HELP_COMMENT = config_data.get(
+            'ADD_HELP_COMMENT',
+            self.ADD_HELP_COMMENT
+        )
+        self.EDITOR = config_data.get(
+            'EDITOR',
+            self.EDITOR
+        )
         self.DEFAULT_DUE_DAYS = config_data.get(
             'DEFAULT_DUE_DAYS',
             self.DEFAULT_DUE_DAYS
@@ -103,7 +150,44 @@ class Settings:
 
     def save(self) -> bool:
         """
-        Save the config file to the home data dir.
+        Save the config file to the home data dir. Also,
+        if enabled, add the help comment to the config.yaml
+        file.
+
+        Returns:
+            bool: Returns True on success.
+        """
+        data_saved = self.save_data()
+        if self.ADD_HELP_COMMENT:
+            help_saved = self.save_add_help_comment()
+        else:
+            help_saved = True
+        return data_saved and help_saved
+
+    def save_add_help_comment(self) -> bool:
+        """
+        Add the help comment to the config.yaml.
+
+        Returns:
+            bool: Returns True on success.
+        """
+        try:
+            with open(self.CONFIGFILE, 'r') as file:
+                original_content = file.read()
+
+            new_content = self.get_help_comment() + original_content
+
+            with open(self.CONFIGFILE, 'w') as file:
+                file.write(new_content)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    def save_data(self) -> bool:
+        """
+        Save the pure variables / data into the
+        config.yaml.
 
         Returns:
             bool: Returns True on success.
