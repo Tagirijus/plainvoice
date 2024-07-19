97315cbe3761096ad2f907c46e7f2a0fd6fb2904
Manuel <info@tagirijus.de>
2024-07-19 16:57:52 +0200

    changed double quotes to single quotes

--- setup.py
+++ setup.py
@@ -4,19 +4,18 @@ from setuptools import setup
 from setuptools import find_packages
 
 _setup = setup(
-    name="plainvoice",
-    version="0.0.1",
-    author="Manuel Senfft",
-    author_email="info@tagirijus.de",
-    description="Creating invoices and documents with a plaintext mindset.",
-    license="MIT",
-    keywords="invoice invoice-generator quote quote-generator",
+    name='plainvoice',
+    version='0.0.1',
+    author='Manuel Senfft',
+    author_email='info@tagirijus.de',
+    description='Creating invoices and documents with a plaintext mindset.',
+    license='MIT',
+    keywords='invoice invoice-generator quote quote-generator',
     packages=find_packages(),
     install_requires=[
         'click~=8.1',
         'jinja2~=3.1',
         'rich~=13.7',
-        'prompt_toolkit~=3.0',
         'weasyprint~=62',
         'PyYAML~=6.0'
     ],
