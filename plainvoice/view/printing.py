b9ba8d924c53d947e812b77fbe968592d39ecf86
Manuel <info@tagirijus.de>
2024-07-19 17:01:23 +0200

    created (or modified) the new Printing class

--- plainvoice/view/printing.py
+++ plainvoice/view/printing.py
@@ -4,42 +4,42 @@ import rich
 
 
 class Printing:
-    """
+    '''
     Simple printing output methods.
-    """
+    '''
 
     @staticmethod
     def print_error(message: str) -> None:
-        """
+        '''
         Print error output.
 
         Args:
             message (str): The message to display.
-        """
-        rich.print(f":warning: [red]{message}[/red]")
+        '''
+        rich.print(f':warning: [red]{message}[/red]')
 
     @staticmethod
     def print_formatted(message: str) -> None:
-        """
+        '''
         Print formatted output.
 
         Args:
             message (str): \
                 The message to display. Can have prompt_toolkit formatting \
                 syntax in the string like <bold> or so.
-        """
+        '''
         rich.print(message)
 
     @staticmethod
     def print_if_verbose(err: Exception) -> None:
-        """
+        '''
         This function checks if "verbose" is enabled and then
         prints out error messages with traceback on level 2
         of verbosity.
 
         Args:
             err (str): The error message to display.
-        """
+        '''
         ctx = click.get_current_context()
         verbose = ctx.obj.get('verbose', 0)
         if verbose >= 1:
@@ -50,51 +50,51 @@ class Printing:
 
     @staticmethod
     def print_info(message: str) -> None:
-        """
+        '''
         Print info output.
 
         Args:
             message (str): The message to display.
-        """
-        rich.print(f"[blue]{message}[/blue]")
+        '''
+        rich.print(f'[blue]{message}[/blue]')
 
     @staticmethod
     def print_items_in_columns(items: list[str], padding: int = 3) -> None:
-        """
+        '''
         Prints the given list in equally spread columns.
 
         Args:
             items (list): The items to print.
             padding (int): The padding between the elements. (default: `3`)
-        """
+        '''
         terminal_width = os.get_terminal_size().columns
         max_item_length = max(len(item) for item in items)
         col_width = max_item_length + padding
         num_cols = terminal_width // col_width
 
         for i, item in enumerate(items):
-            end_char = "\n" if (i + 1) % num_cols == 0 else " " * padding
-            print(f"{item:<{max_item_length}}", end=end_char)
+            end_char = '\n' if (i + 1) % num_cols == 0 else ' ' * padding
+            print(f'{item:<{max_item_length}}', end=end_char)
 
         if len(items) % num_cols != 0:
             print()
 
     @staticmethod
     def print_success(message: str) -> None:
-        """
+        '''
         Print success output.
 
         Args:
             message (str): The message to display.
-        """
-        rich.print(f"[green]{message}[/green]")
+        '''
+        rich.print(f'[green]{message}[/green]')
 
     @staticmethod
     def print_warning(message: str) -> None:
-        """
+        '''
         Print warning output.
 
         Args:
             message (str): The message to display.
-        """
-        rich.print(f"[yellow]{message}[/yellow]")
+        '''
+        rich.print(f'[yellow]{message}[/yellow]')
