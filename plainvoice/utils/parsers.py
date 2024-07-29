58576b4a5afd9184e7fe379fc30468f77f78abab
Manuel <info@tagirijus.de>
2024-07-29 16:22:52 +0200

    moved quantity parsing to new Quantity class

--- plainvoice/utils/parsers.py
+++ plainvoice/utils/parsers.py
@@ -1,7 +1,5 @@
 from decimal import Decimal
 
-import re
-
 
 # ### ### ### ### ### ### #
 # Posting related parsers #
@@ -49,67 +47,3 @@ def parse_vat_string(vat_string: str) -> tuple[Decimal, str]:
     elif len(dec_str) > 3:
         dec_str = str(round(dec * 100, 2)) + ' %'
     return dec, dec_str
-
-
-def split_quantity_string(quantity_string: str) -> tuple[str, str]:
-    '''
-    This function splits a given quantity string into the number
-    part and the suffix part.
-
-    Examples:
-      "1.5"       ->  ("1.5", "")
-      "1.5h"      ->  ("1.5", "h")
-      "1:05"      ->  ("1:05", "")
-      "1:30 min"  ->  ("1:30", "min")
-
-    The number part still is a string!! It can be converted
-    into a Decimal with the timestring_to_decimal() function.
-
-    Args:
-        quantity_string (str): The string to 'parse'.
-
-    Returns:
-        tuple: \
-            - str: The numer of the quantity string.
-            - str: The quantity descriptor of the string.
-
-    Raises:
-        ValueError: Error, if 'parsing' might not be possible.
-    '''
-    pattern = r'^([\d.:]+)\s*([^\d.:]*)$'
-    match = re.match(pattern, quantity_string.strip())
-
-    if match:
-        number = match.group(1)
-        suffix = match.group(2).strip()
-
-        return number, suffix
-    else:
-        raise ValueError(f'Quantity format not possible: {quantity_string}')
-
-
-def timestring_to_decimal(timestring: str) -> Decimal:
-    '''
-    This function is capable to convert a possible time notation
-    like "1:30" into a proper Decimal(1.5). If there is no
-    colon, the string will be converted to a Decimal().
-
-    Args:
-        timestring (str): The string like '1:30'.
-
-    Returns:
-        Decimal: The decimal, which gets converted from the string.
-
-    Raises:
-        ValueError: Error if string is not convertable.
-    '''
-    if ':' in timestring:
-        splitted = timestring.split(':')
-        if len(splitted) > 1:
-            a = Decimal(splitted[0].strip())
-            b = Decimal(splitted[1].strip())
-            return a + (b / 60)
-        else:
-            raise ValueError(f'Quantity has invalid time string: {timestring}')
-    else:
-        return Decimal(timestring)
