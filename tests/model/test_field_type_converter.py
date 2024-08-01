7eb1d1ba2ddd6bc8239c911be1313281caa212b3
Manuel <info@tagirijus.de>
2024-08-01 11:21:11 +0200

    commented tests for FieldTypeConverter

--- tests/model/test_field_type_converter.py
+++ tests/model/test_field_type_converter.py
@@ -2,10 +2,15 @@ from plainvoice.model.field.field_type_converter import FieldTypeConverter
 
 
 def test_converting():
+    # prepare some variables to test with
     field_type_converter = FieldTypeConverter('int', int, 0, str, '0')
 
-    to_internal = field_type_converter.convert_to_internal(1)
+    # the FieldTypeConverter should use the str to convert to
+    # int for the internal type
+    to_internal = field_type_converter.convert_to_internal('1')
     assert to_internal == 1
 
+    # vice verse the FieldTypeConverter should use the int to
+    # convert to int for the readable type
     to_readable = field_type_converter.convert_to_readable(to_internal)
     assert to_readable == '1'
