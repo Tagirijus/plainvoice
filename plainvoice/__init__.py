1373bbcff74c593f5d1e835bade77d33c8c6dc37
Manuel <info@tagirijus.de>
2024-08-02 23:25:10 +0200

    re-added Client to the export

--- plainvoice/__init__.py
+++ plainvoice/__init__.py
@@ -5,6 +5,7 @@ Author: Manuel Senfft (www.tagirijus.de)
 '''
 
 from .model.config import Config
+from .model.client.client import Client
 from .model.data.data_model import DataModel
 from .model.file.file import File
 from .model.quantity.percentage import Percentage
@@ -14,6 +15,7 @@ from .model.quantity.quantity import Quantity
 
 __all__ = [
     'Config',
+    'Client',
     'File',
     'DataModel',
     'Percentage',
