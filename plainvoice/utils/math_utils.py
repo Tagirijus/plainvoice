e4133f013733ac413c1fcc855c476f2a3f2aece2
Manuel <info@tagirijus.de>
2024-08-05 15:43:47 +0200

    removed method, which was not needed anymore

--- plainvoice/utils/math_utils.py
+++ plainvoice/utils/math_utils.py
@@ -1,19 +1,6 @@
 from decimal import Decimal, ROUND_HALF_UP
 
 
-def round2(dec: Decimal) -> Decimal:
-    '''
-    Rounds the given Decimal to two digits after the decimal.
-
-    Args:
-        dec (Decimal): The Decimal object input.
-
-    Returns:
-        Decimal: The rounded Decimal output object.
-    '''
-    return dec.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
-
-
 def is_convertible_to_int(string):
     '''
     Checks, if the given string can be converted into an integer.
