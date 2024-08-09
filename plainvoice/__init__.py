d1455d92387e79f40ec6615646d9e2dc2318a9ae
Manuel <info@tagirijus.de>
2024-08-09 15:29:55 +0200

    added DocumentRepository class

--- plainvoice/__init__.py
+++ plainvoice/__init__.py
@@ -9,6 +9,7 @@ from .model.client.client import Client
 from .model.data.data_model import DataModel
 from .model.data.data_repository import DataRepository
 from .model.document.document import Document
+from .model.document.document_repository import DocumentRepository
 from .model.file.file import File
 from .model.posting.posting import Posting
 from .model.posting.postings_list import PostingsList
@@ -23,6 +24,7 @@ __all__ = [
     'DataModel',
     'DataRepository',
     'Document',
+    'DocumentRepository',
     'File',
     'Posting',
     'PostingsList',
