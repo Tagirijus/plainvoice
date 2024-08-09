baecc0229d37c607a1c32be847dfe963ac0924aa
Manuel <info@tagirijus.de>
2024-08-09 16:01:00 +0200

    modified my tests in pytest to work with conftest.py

--- tests/model/test_file.py
+++ tests/model/test_file.py
@@ -1,43 +1,27 @@
 from plainvoice.model.file.file import File
 
 import os
-import pytest
-
-FILE_TEST_PATH = './_plainvoice_test_file.yaml'
-FILE_TEST_CONTENT = 'plainvoice testing: File class'
 
 
-@pytest.fixture
-def temp_file():
-    '''
-    This method prepares a file to test on and
-    removes it again later after the test.
-    '''
-    file_path = FILE_TEST_PATH
-
-    with open(file_path, 'w') as f:
-        f.write(FILE_TEST_CONTENT)
-
-    yield file_path
-
-    if os.path.exists(file_path):
-        os.remove(file_path)
-
-
-def test_copy(temp_file):
+def test_copy(test_temp_file):
     # prepare a filename to copy to
     # and copy it
-    temp_file_copy = temp_file + '.copy'
+    temp_file_content = 'plainvoice testing File class ...'
+    temp_file_name = '_plainvoice_File_test.txt'
+    temp_file = test_temp_file(temp_file_name, temp_file_content)
+    temp_file_name_copy = temp_file + '.copy'
+
     file = File()
-    file.copy(temp_file, temp_file_copy)
-    if os.path.exists(temp_file_copy):
-        with open(temp_file, 'r') as test_file:
-            data = test_file.read()
-        os.remove(temp_file_copy)
+    file.copy(temp_file, temp_file_name_copy)
+
+    if os.path.exists(temp_file_name_copy):
+        with open(temp_file, 'r') as temp_file:
+            data = temp_file.read()
+        os.remove(temp_file_name_copy)
 
     # the content of the copied file should be
     # the same as the original one
-    assert data == FILE_TEST_CONTENT
+    assert data == temp_file_content
 
 
 def test_get_extension_get_folder():
