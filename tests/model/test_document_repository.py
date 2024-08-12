aa48e06568f3e66676d84d69fcf28b3e9a53116d
Manuel <info@tagirijus.de>
2024-08-12 15:21:10 +0200

    added missing test data

--- tests/model/test_document_repository.py
+++ tests/model/test_document_repository.py
@@ -1,4 +1,5 @@
 from plainvoice.model.document.document_repository import DocumentRepository
+from plainvoice.model.document.document import Document
 
 
 def test_document_from_absolute_filename(test_data_folder, test_data_file):
@@ -39,3 +40,27 @@ def test_document_from_name(test_data_folder):
     assert doc.is_visible() is False
     assert doc.get_fixed('title', True) == 'invoice #1'
     assert doc.get_additional('company') == 'Plainvoice Inc.'
+
+
+def test_document_linking(test_data_folder):
+    # set the test data folder
+    test_folder = test_data_folder('document_repository')
+    types_folder = test_folder + '/types'
+
+    # instantiate the document repository
+    doc_repo = DocumentRepository('invoice', types_folder)
+
+    # now load a document with the filename "invoice_1"
+    doc = doc_repo.load_document_from_name('invoice_1')
+
+    # now get the linked client_1
+    linked_client = doc_repo.get_links_from_document(doc, False)[0]
+
+    # WEITER HIER
+    # get_links_from_document() mit separatem getter, der Document bekommt
+    # und separatem getter für strings
+    if isinstance(linked_client, Document):
+        assert linked_client.get_fixed('first_name', True) == 'manu'
+        assert linked_client.get_fixed('last_name', True) == 'nunu'
+    else:
+        raise TypeError('linked_client is no Document')
