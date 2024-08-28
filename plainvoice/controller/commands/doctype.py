d9c838f1c0b28f549e8291c0745cbcbb7d23fbe9
Manuel <info@tagirijus.de>
2024-08-28 12:25:13 +0200

    document type edit command ready

--- plainvoice/controller/commands/doctype.py
+++ plainvoice/controller/commands/doctype.py
@@ -5,6 +5,11 @@ This module holds all the commands for the document type
 handling.
 '''
 
+from plainvoice.model.config import Config
+from plainvoice.model.document.document_type_repository import \
+    DocumentTypeRepository
+from plainvoice.utils import file_utils
+
 import click
 
 
@@ -20,4 +25,11 @@ def type():
 @click.argument('name')
 def type_edit(name):
     """Create and / or edit a document type."""
-    print(name)
+    doc_type_repo = DocumentTypeRepository(
+        str(Config().get('types_folder'))
+    )
+    if name not in doc_type_repo.get_list(True).keys():
+        doc_type_repo.create_type(name)
+    file_utils.open_in_editor(
+        doc_type_repo.get_absolute_filename(name)
+    )
