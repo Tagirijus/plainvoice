f314b38d752aaebe6c8799f1c15500c3c56c00c1
Manuel <info@tagirijus.de>
2024-08-30 08:19:32 +0200

    moved document commands to DocumentController

--- plainvoice/controller/commands/document.py
+++ plainvoice/controller/commands/document.py
@@ -4,12 +4,7 @@ document module
 This module holds all the commands for the document handling.
 '''
 
-from plainvoice.controller.io_facade.io_facade import IOFacade as io
-from plainvoice.model.config import Config
-from plainvoice.model.script.script_repository import ScriptRepository
-from plainvoice.model.template.template_repository import TemplateRepository
-from plainvoice.utils import doc_utils
-from plainvoice.utils import file_utils
+from plainvoice.controller.document_controller import DocumentController
 
 import click
 
@@ -18,9 +13,9 @@ import click
 @click.group(context_settings=dict(help_option_names=['-h', '--help']))
 @click.pass_context
 def doc(ctx, type):
-    """
+    '''
     Do stuff with documents.
-    """
+    '''
     ctx = ctx
     ctx.obj['type'] = type
 
@@ -29,87 +24,43 @@ def doc(ctx, type):
 @click.argument('name')
 @click.pass_context
 def doc_edit(ctx, name):
-    """Edit a document, if it exists."""
-    doc_repo = doc_utils.get_doc_repo()
-    doc_type, name = doc_utils.get_doc_type_and_name(ctx.obj['type'], name)
-    if doc_repo.exists(doc_type, name):
-        file_utils.open_in_editor(
-            doc_repo.get_filename(doc_type, name)
-        )
-    else:
-        io.print(f'Document "{name}" not found!', 'warning')
+    '''
+    Edit a document, if it exists. Also update its fixed fields
+    according to the document type.
+    '''
+    DocumentController().edit(ctx.obj['type'], name)
 
 
 @doc.command('hide')
 @click.argument('name')
 @click.pass_context
 def doc_hide(ctx, name):
-    """Hide a document."""
-    doc_repo = doc_utils.get_doc_repo()
-    doc_type, name = doc_utils.get_doc_type_and_name(ctx.obj['type'], name)
-    if doc_repo.exists(doc_type, name):
-        doc = doc_repo.load(name, doc_type)
-        doc.hide()
-        doc_repo.save(doc)
-        io.print(f'Document "{name}" now hidden.', 'success')
-    else:
-        io.print(f'Document "{name}" not found.', 'warning')
+    '''Hide a document.'''
+    DocumentController().change_visibility(ctx.obj['type'], name, True)
 
 
 @doc.command('list')
 @click.option('-a', '--show-all', is_flag=True, help='Also list hidden items')
 @click.pass_context
 def doc_list(ctx, show_all):
-    """List available and visible documents."""
-    doc_repo = doc_utils.get_doc_repo()
-    type = ctx.obj['type']
-    # show_all has to be inverted, since the method will accept the keyword
-    # show_only_visible, but for the cli interface I want to have the
-    # flag to be used on showing all; thus logically it has to be named
-    # different and the value has to be inverted then. bit confusing, but
-    # hopefully no biggie after all ...
-    docs_list = doc_repo.get_list(type, not show_all)
-    if docs_list:
-        io.print_list(
-            sorted(
-                doc_repo.get_list(type, not show_all).keys()
-            )
-        )
-    else:
-        io.print(f'No documents found for type "{type}".', 'warning')
+    '''List available and visible documents.'''
+    DocumentController().list(ctx.obj['type'], show_all)
 
 
 @doc.command('new')
 @click.argument('name')
 @click.pass_context
 def doc_new(ctx, name):
-    """Create a new document or edit it if it exists already."""
-    doc_repo = doc_utils.get_doc_repo()
-    doc_type, name = doc_utils.get_doc_type_and_name(ctx.obj['type'], name)
-    if doc_type is None:
-        io.print(f'Please specify a document type with -t/--type!', 'warning')
-    else:
-        doc_repo.create_document(doc_type, name)
-        file_utils.open_in_editor(
-            doc_repo.get_filename(doc_type, name)
-        )
+    '''Create a new document or edit it if it exists already.'''
+    DocumentController().new(ctx.obj['type'], name)
 
 
 @doc.command('remove')
 @click.argument('name')
 @click.pass_context
 def doc_remove(ctx, name):
-    """Remove a document."""
-    doc_repo = doc_utils.get_doc_repo()
-    doc_type, name = doc_utils.get_doc_type_and_name(ctx.obj['type'], name)
-    if doc_repo.exists(doc_type, name):
-        if io.ask_yes_no(f'Remove document "{name}"?'):
-            doc_repo.remove(doc_type, name)
-            io.print(f'Document "{name}" removed.', 'success')
-        else:
-            io.print(f'Document "{name}" not removed.', 'warning')
-    else:
-        io.print(f'Document "{name}" not found.', 'warning')
+    '''Remove a document.'''
+    DocumentController().remove(ctx.obj['type'], name)
 
 
 @doc.command('render')
@@ -118,31 +69,14 @@ def doc_remove(ctx, name):
 @click.option('-o', '--output-file', default='', help='The output file')
 @click.pass_context
 def doc_render(ctx, name, template, output_file):
-    """Render a document."""
-    doc_repo = doc_utils.get_doc_repo()
-    user = doc_utils.get_user(ctx.obj['user'])
-    doc_type, name = doc_utils.get_doc_type_and_name(ctx.obj['type'], name)
-    template_repo = TemplateRepository(str(Config().get('templates_folder')))
-    if template is None:
-        io.print(f'Specify a template. Choose one of those:', 'warning')
-        io.print_list(sorted(template_repo.get_template_names()))
-    else:
-        if doc_repo.exists(doc_type, name):
-            # create the render engine; import only on demand,
-            # since weasyprint is slow loading
-            from plainvoice.view.render import Render
-            render = Render(str(Config().get('templates_folder')))
-
-            # load the document and render it
-            doc = doc_repo.load(name, doc_type)
-            if render.render(template, doc, user, output_file):
-                io.print(
-                    f'Rendered document "{name}" successfully.', 'success'
-                )
-            else:
-                io.print(f'Rendering document "{name}" went wrong.', 'error')
-        else:
-            io.print(f'Document "{name}" not found.', 'warning')
+    '''Render a document.'''
+    DocumentController().render(
+        ctx.obj['type'],
+        name,
+        template,
+        ctx.obj['user'],
+        output_file
+    )
 
 
 @doc.command('script')
@@ -156,62 +90,31 @@ def doc_render(ctx, name, template, output_file):
 )
 @click.pass_context
 def doc_script(ctx, name, script, quiet):
-    """Execute a script on the given document."""
-    doc_repo = doc_utils.get_doc_repo()
-    user = doc_utils.get_user(ctx.obj['user'])
-    doc_type, name = doc_utils.get_doc_type_and_name(ctx.obj['type'], name)
-    script_repo = ScriptRepository(str(Config().get('scripts_folder')))
-    if script is None:
-        io.print(f'Specify a script. Choose one of those:', 'warning')
-        io.print_list(sorted(script_repo.get_script_names()))
-    else:
-        if doc_repo.exists(doc_type, name):
-            # get script
-            script_obj = script_repo.load(script)
-
-            # load the document and pass it to the script
-            doc = doc_repo.load(name, doc_type)
-            if not quiet:
-                io.print(
-                    f'Running script "{script}" on document "{name}" ...',
-                    'success'
-                )
-            script_obj.run(doc, user)
-        else:
-            io.print(f'Document "{name}" not found.', 'warning')
+    '''Execute a script on the given document.'''
+    DocumentController().script(
+        ctx.obj['type'],
+        name,
+        script,
+        ctx.obj['user'],
+        quiet
+    )
 
 
 @doc.command('show')
 @click.argument('name')
 @click.pass_context
 def doc_show(ctx, name):
-    """Show a document."""
-    doc_repo = doc_utils.get_doc_repo()
-    doc_type, name = doc_utils.get_doc_type_and_name(ctx.obj['type'], name)
-    if doc_repo.exists(doc_type, name):
-        doc = doc_repo.load(name, doc_type)
-        doc.show()
-        doc_repo.save(doc)
-        io.print(f'Document "{name}" now visible.', 'success')
-    else:
-        io.print(f'Document "{name}" not found.', 'warning')
+    '''Show a document.'''
+    DocumentController().change_visibility(ctx.obj['type'], name, False)
 
 
 @doc.command('update')
 @click.argument('name')
 @click.pass_context
 def doc_update(ctx, name):
-    """
+    '''
     Update a document with new fixed fields according to a maybe
-    updated docuemnt type. After that edit it immediately.
-    """
-    doc_repo = doc_utils.get_doc_repo()
-    doc_type, name = doc_utils.get_doc_type_and_name(ctx.obj['type'], name)
-    if doc_repo.exists(doc_type, name):
-        doc = doc_repo.load(name, doc_type)
-        doc_repo.save(doc)
-        file_utils.open_in_editor(
-            doc.get_filename()
-        )
-    else:
-        io.print(f'Document "{name}" not found.', 'warning')
+    updated docuemnt type. After that edit it immediately. Basically
+    this is just an alias for the edit command.
+    '''
+    DocumentController().edit(ctx.obj['type'], name)
---
+++ plainvoice/controller/document_controller.py
@@ -0,0 +1,246 @@
+'''
+DocumentController class
+
+Handles Document managing.
+'''
+
+from plainvoice.controller.io_facade.io_facade import IOFacade as io
+from plainvoice.model.config import Config
+from plainvoice.model.script.script_repository import ScriptRepository
+from plainvoice.model.template.template_repository import TemplateRepository
+from plainvoice.utils import doc_utils
+from plainvoice.utils import file_utils
+
+
+class DocumentController:
+    '''
+    Handles Document managing.
+    '''
+
+    def __init__(self):
+        '''
+        Handles Document managing.
+        '''
+        self.doc_repo = doc_utils.get_doc_repo()
+
+    def change_visibility(
+        self,
+        doc_typename: str,
+        name: str,
+        hide: bool = True
+    ) -> None:
+        '''
+        Hide the document with the given type and name.
+
+        Args:
+            doc_typename (str): The name of the document type.
+            name (str): The name of the document.
+            hide (bool): If True, hide the document.
+        '''
+        doc_typename, name = doc_utils.get_doc_type_and_name(
+            doc_typename,
+            name
+        )
+        if self.doc_repo.exists(doc_typename, name):
+            doc = self.doc_repo.load(name, doc_typename)
+            if hide:
+                doc.hide()
+                io.print(f'Document "{name}" now hidden.', 'success')
+            else:
+                doc.show()
+                io.print(f'Document "{name}" now visible.', 'success')
+            self.doc_repo.save(doc)
+        else:
+            io.print(f'Document "{name}" not found.', 'warning')
+
+    def edit(self, doc_typename: str, name: str) -> None:
+        '''
+        Edit the document with the given type and name. Also it
+        will update the values, if the document type, for example,
+        got fixed fields changed or so.
+
+        Args:
+            doc_typename (str): The name of the document type.
+            name (str): The name of the document.
+        '''
+        doc_typename, name = doc_utils.get_doc_type_and_name(
+            doc_typename,
+            name
+        )
+        if self.doc_repo.exists(doc_typename, name):
+            doc = self.doc_repo.load(name, doc_typename)
+            self.doc_repo.save(doc)
+            file_utils.open_in_editor(
+                self.doc_repo.get_filename(doc_typename, name)
+            )
+        else:
+            io.print(f'Document "{name}" not found!', 'warning')
+
+    def list(self, doc_typename: str, show_all: bool) -> None:
+        '''
+        List documents of a certain type.
+
+        Args:
+            doc_typename (str): The name of the document type.
+            show_all (bool): If True, shows also hidden types.
+        '''
+        # show_all is on the show_only_visible argument; thus
+        # it has to be inverted to act correct
+        docs_list = self.doc_repo.get_list(doc_typename, not show_all)
+        if docs_list:
+            io.print_list(
+                sorted(
+                    self.doc_repo.get_list(doc_typename, not show_all).keys()
+                )
+            )
+        else:
+            io.print(
+                f'No documents found for type "{doc_typename}".',
+                'warning'
+            )
+
+    def new(self, doc_typename: str, name: str) -> None:
+        '''
+        Create a new document with the given type and name.
+
+        Args:
+            doc_typename (str): The name of the document type.
+            name (str): The name of the document.
+        '''
+        doc_typename, name = doc_utils.get_doc_type_and_name(
+            doc_typename,
+            name
+        )
+        if doc_typename is None:
+            io.print(
+                f'Please specify a document type with -t/--type!',
+                'warning'
+            )
+        else:
+            self.doc_repo.create_document(doc_typename, name)
+            file_utils.open_in_editor(
+                self.doc_repo.get_filename(doc_typename, name)
+            )
+
+    def remove(self, doc_typename: str, name: str) -> None:
+        '''
+        Remove the document with the given type and name.
+
+        Args:
+            doc_typename (str): The name of the document type.
+            name (str): The name of the document.
+        '''
+        doc_typename, name = doc_utils.get_doc_type_and_name(
+            doc_typename,
+            name
+        )
+        if self.doc_repo.exists(doc_typename, name):
+            if io.ask_yes_no(f'Remove document "{name}"?'):
+                self.doc_repo.remove(doc_typename, name)
+                io.print(f'Document "{name}" removed.', 'success')
+            else:
+                io.print(f'Document "{name}" not removed.', 'warning')
+        else:
+            io.print(f'Document "{name}" not found.', 'warning')
+
+    def render(
+        self,
+        doc_typename: str,
+        name: str,
+        template_name: str | None,
+        user_name: str = '',
+        output_file: str = ''
+    ) -> None:
+        '''
+        Render the document with the given type and name and the
+        given template name. Optionally out to the given
+        output file.
+
+        Args:
+            doc_typename (str): The name of the document type.
+            name (str): The name of the document.
+            template_name (str): The name of the template.
+            user_name (str): Optional the user name to use.
+            output_file (str): Optional the output filename to save to.
+        '''
+        doc_typename, name = doc_utils.get_doc_type_and_name(
+            doc_typename,
+            name
+        )
+        user = doc_utils.get_user(user_name)
+        template_repo = TemplateRepository(
+            str(Config().get('templates_folder'))
+        )
+        if template_name is None:
+            io.print(f'Specify a template. Choose one of those:', 'warning')
+            io.print_list(sorted(template_repo.get_template_names()))
+        else:
+            if self.doc_repo.exists(doc_typename, name):
+                # create the render engine; import only on demand,
+                # since weasyprint is slow loading
+                from plainvoice.view.render import Render
+                render = Render(str(Config().get('templates_folder')))
+
+                # load the document and render it
+                doc = self.doc_repo.load(name, doc_typename)
+                if render.render(template_name, doc, user, output_file):
+                    io.print(
+                        f'Rendered document "{name}" successfully.',
+                        'success'
+                    )
+                else:
+                    io.print(
+                        f'Rendering document "{name}" went wrong.',
+                        'error'
+                    )
+            else:
+                io.print(f'Document "{name}" not found.', 'warning')
+
+    def script(
+        self,
+        doc_typename: str,
+        name: str,
+        script_name: str | None,
+        user_name: str = '',
+        quiet: bool = False
+    ) -> None:
+        '''
+        Execute a script with the document with the given type and name
+        and the given script name. You can also set another user_name
+        so that some other user instead of the default one will be used
+        so that the variable "user" can have different values in the
+        script.
+
+        Args:
+            doc_typename (str): The name of the document type.
+            name (str): The name of the document.
+            script_name (str): The name of the script.
+            user_name (str): Optional the user name to use.
+            quiet (bool): If True no output from plainvoice will come, \
+                only from the script itself.
+        '''
+        doc_typename, name = doc_utils.get_doc_type_and_name(
+            doc_typename,
+            name
+        )
+        user = doc_utils.get_user(user_name)
+        script_repo = ScriptRepository(str(Config().get('scripts_folder')))
+        if script_name is None:
+            io.print(f'Specify a script. Choose one of those:', 'warning')
+            io.print_list(sorted(script_repo.get_script_names()))
+        else:
+            if self.doc_repo.exists(doc_typename, name):
+                # get script
+                script_obj = script_repo.load(script_name)
+
+                # load the document and pass it to the script
+                doc = self.doc_repo.load(name, doc_typename)
+                if not quiet:
+                    io.print(
+                        f'Running script "{script_name}" on document "{name}"'
+                        + ' ...',
+                        'success'
+                    )
+                script_obj.run(doc, user)
+            else:
+                io.print(f'Document "{name}" not found.', 'warning')
