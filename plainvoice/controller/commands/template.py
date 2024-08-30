91f5bdde916404bbabd9cf977e405590acb684f5
Manuel <info@tagirijus.de>
2024-08-30 08:28:23 +0200

    moved template commands to TemplateController

--- plainvoice/controller/commands/template.py
+++ plainvoice/controller/commands/template.py
@@ -5,67 +5,45 @@ This module holds all the commands for the document type
 handling.
 '''
 
-from plainvoice.controller.io_facade.io_facade import IOFacade as io
-from plainvoice.model.config import Config
-from plainvoice.model.template.template_repository import TemplateRepository
-from plainvoice.utils import file_utils
+from plainvoice.controller.template_controller import TemplateController
 
 import click
-import os
 
 
 @click.group(context_settings=dict(help_option_names=['-h', '--help']))
 @click.pass_context
 def template(ctx):
-    """
+    '''
     Create or edit templates.
-    """
+    '''
     ctx = ctx
 
 
 @template.command('edit')
 @click.argument('name')
 def template_edit(name):
-    """Create and / or edit a document type."""
-    template_repo = TemplateRepository(str(Config().get('templates_folder')))
-    file_name = template_repo.get_absolute_filename(name)
-    if not os.path.exists(file_name):
-        template_repo.create_template(name)
-    file_utils.open_in_editor(
-        file_name
-    )
+    '''Create and / or edit a document type.'''
+    TemplateController().edit(name)
 
 
 @template.command('list')
 def template_list():
-    """List available templates."""
-    template_repo = TemplateRepository(
-        str(Config().get('templates_folder'))
-    )
-    io.print_list(sorted(template_repo.get_template_names()))
+    '''List available templates.'''
+    TemplateController().list()
 
 
 @template.command('new')
 @click.argument('name')
-@click.pass_context
-def template_new(ctx, name):
-    """
+def template_new(name):
+    '''
     Create a new templates or edit it if it exists already. It is basically
     an alias for the "edit" command.
-    """
-    ctx.invoke(template_edit, name=name)
+    '''
+    TemplateController().edit(name)
 
 
 @template.command('remove')
 @click.argument('name')
 def template_remove(name):
-    """Remove a template."""
-    template_repo = TemplateRepository(str(Config().get('templates_folder')))
-    if template_repo.exists(name):
-        if io.ask_yes_no(f'Remove template "{name}"?'):
-            template_repo.remove(name)
-            io.print(f'Template "{name}" removed.', 'success')
-        else:
-            io.print(f'Template "{name}" not removed.', 'warning')
-    else:
-        io.print(f'Template "{name}" not found.', 'warning')
+    '''Remove a template.'''
+    TemplateController().remove(name)
---
+++ plainvoice/controller/template_controller.py
@@ -0,0 +1,62 @@
+'''
+DocumentTypeController class
+
+Handles DocumentType managing.
+'''
+
+from plainvoice.controller.io_facade.io_facade import IOFacade as io
+from plainvoice.model.config import Config
+from plainvoice.model.template.template_repository import TemplateRepository
+from plainvoice.utils import file_utils
+
+import os
+
+
+class TemplateController:
+    '''
+    Handles Template managing.
+    '''
+
+    def __init__(self):
+        '''
+        Handles Templates managing.
+        '''
+        self.template_repo = TemplateRepository(
+            str(Config().get('templates_folder'))
+        )
+
+    def edit(self, name: str) -> None:
+        '''
+        Edit the template with the given name.
+
+        Args:
+            name (str): The name of the template.
+        '''
+        file_name = self.template_repo.get_absolute_filename(name)
+        if not os.path.exists(file_name):
+            self.template_repo.create_template(name)
+        file_utils.open_in_editor(
+            file_name
+        )
+
+    def list(self) -> None:
+        '''
+        List templates.
+        '''
+        io.print_list(sorted(self.template_repo.get_template_names()))
+
+    def remove(self, name: str) -> None:
+        '''
+        Remove the document type with the given name.
+
+        Args:
+            name (str): The name of the document type.
+        '''
+        if self.template_repo.exists(name):
+            if io.ask_yes_no(f'Remove template "{name}"?'):
+                self.template_repo.remove(name)
+                io.print(f'Template "{name}" removed.', 'success')
+            else:
+                io.print(f'Template "{name}" not removed.', 'warning')
+        else:
+            io.print(f'Template "{name}" not found.', 'warning')
