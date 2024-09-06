a016cfb59df22c5bb02bbf4721feb34a8ccaf475
Manuel <info@tagirijus.de>
2024-09-06 13:39:47 +0200

    extended default postings from dict conversion test

--- tests/model/test_posting.py
+++ tests/model/test_posting.py
@@ -54,6 +54,15 @@ def test_posting_defaults():
     assert posting.get_fixed('quantity', True) == '1'
     assert posting.get_fixed('vat', True) == '0 %'
 
+    # also check what happens if I convert a Posting
+    # from dict, while the dict is empty
+    posting_2 = Posting().instance_from_dict({})
+    assert posting_2.get_fixed('title', True) == ''
+    assert posting_2.get_fixed('detail', True) == ''
+    assert posting_2.get_fixed('unit_price', True) == '1.00 €'
+    assert posting_2.get_fixed('quantity', True) == '1'
+    assert posting_2.get_fixed('vat', True) == '0 %'
+
 
 def test_math_operations():
     # create a posting instance
